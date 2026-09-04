5360944c7a9e0c0fc1a97f8f92038b698a9e7f9d
canyon-developer <glin937@insite.4cd.edu>
2026-09-04 04:26:35 +0000

    Restaurant structure declaration

---
+++ lab3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+struct Restaurant {
+    string name;
+    string address;
+    string style;
+    string petAllowed;
+    string onlineOrderOffered;
+};
+
+Restaurant addRestaurant();
+void restaurantInfo(Restaurant& restaurant)
+
+int main() {
+    
+}
