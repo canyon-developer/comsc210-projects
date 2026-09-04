27448ec138ca6b41db5ae71ad582317480ec6ded
canyon-developer <glin937@insite.4cd.edu>
2026-09-04 04:39:46 +0000

    addRestaurant definition

--- lab3.cpp
+++ lab3.cpp
@@ -11,8 +11,36 @@ struct Restaurant {
 };
 
 Restaurant addRestaurant();
-void restaurantInfo(Restaurant& restaurant)
+void restaurantInfo(Restaurant& restaurant);
 
 int main() {
-    
+    const int NUMBER = 4;
+    Restaurant restaurants[NUMBER]; // 4 restaurnants
+
+    for (int i = 0; i < 2; i++) {
+        cout << "Populate restaurant info for the " << i + 1 << " restaurant:" << endl;
+        restaurants[i] = addRestaurant();
+    }
+
+    return 0;
+}
+
+Restaurant addRestaurant() {
+    Restaurant restaurant;
+    cout << "What is the name: ";
+    getline(cin, restaurant.name);
+
+    cout << "What is its address: ";
+    getline(cin, restaurant.address);
+
+    cout << "What is its style: ";
+    getline(cin, restaurant.style);
+
+    cout << "Is pet allowed (y/n)?: ";
+    getline(cin, restaurant.petAllowed);
+
+    cout << "Does it provide online ordering (y/n)?: ";
+    getline(cin, restaurant.onlineOrderOffered);
+
+    return restaurant;
 }
