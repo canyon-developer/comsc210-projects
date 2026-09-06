a89f417a3b3aa493cb7cfaf8811d8c8579c75410
canyon-developer <glin937@insite.4cd.edu>
2026-09-06 04:11:30 +0000

    lab4 - Color vector

--- lab4.cpp
+++ lab4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <vector>
 
 using namespace std;
 
@@ -10,8 +11,13 @@ struct Color {
 };
 
 int main() {
-    Color c = {100, 150, 200};
+    vector<Color> colors;
+    colors.push_back({100, 150, 200});
     cout << left << setw(10) << "Color#" << setw(10) << "R value" << setw(10) << "G value" << setw(10) << "B value" << endl;
     cout << "--------------------------------------" << endl;
-    cout << left << setw(10) << 1 << setw(10) << c.red << setw(10) << c.green << setw(10) << c.blue << endl;
+    
+    for (int i = 0; i < 1; i++) {
+      Color c = colors[i];
+      cout << left << setw(10) << i + 1 << setw(10) << c.red << setw(10) << c.green << setw(10) << c.blue << endl;
+    }
 }
