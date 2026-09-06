6b2aaf0d2be1136042aeb2d4510100ef6844ba96
canyon-developer <glin937@insite.4cd.edu>
2026-09-06 03:56:48 +0000

    lab4 - milestone1 - cout format

--- lab4.cpp
+++ lab4.cpp
@@ -9,5 +9,8 @@ struct Color {
 };
 
 int main() {
-
+    Color c = {100, 150, 200};
+    cout << "Color#    " << "R value    " << "G value    " << "B value" << endl;
+    cout << "---------------------------------------------" << endl;
+    cout << "         " << c.red << "         " << c.green << "         " << c.blue << endl;
 }
