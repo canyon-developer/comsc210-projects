0e0562397d59bc8e5bc4b3d16519f73fb61c06ec
canyon-developer <glin937@insite.4cd.edu>
2026-09-13 04:14:37 +0000

    lab7 - implement reverseArray

--- lab7.cpp
+++ lab7.cpp
@@ -3,20 +3,24 @@
 
 using namespace std;
 
-void reverseArray(string *array, int size) {
-  int j = size - 1;
-  for (int i = 0; i < j; i++) {
-    string s = array[i];
-    array[i] = array[j];
-    array[j] = s;
-    j--;
-  }
+string* reverseArray(string *array, int size) {
+    int j = size - 1;
+    for (int i = 0; i < j; i++) {
+      string s = array[i];
+      array[i] = array[j];
+      array[j] = s;
+     j--;
+    }
 
-  return array;
+    return array;
 }
 
 int main() {
   const int SIZE = 5;
   string *array = new string[SIZE];
-
+  array[0] = "Leo";
+  array[1] = "John";
+  array[2] = "Kevin";
+  array[3] = "Amy";
+  array[4] = "Kytelyn";
 }
